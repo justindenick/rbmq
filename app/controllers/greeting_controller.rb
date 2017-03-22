8f3d0c4cb7c0f3f1a984b8272a5d6d80e2ca64ad
justindenick <justin.denick@gmail.com>
2017-03-22 01:53:37 -0400

    adding things model and theme

--- app/controllers/greeting_controller.rb
+++ app/controllers/greeting_controller.rb
@@ -2,7 +2,8 @@ class GreetingController < ApplicationController
 
   # GET /welcome
   def index
-
+    form_tag=["name","sauce"]
+    flash[:notice] = "Your IP is " + request.remote_ip
   end
 
 end
